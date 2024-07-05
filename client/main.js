ddf85d4fb5d37f4ac6fbddcc6e2884c6afbe4005
Yatsuki-ish <96803489+Yatsuki-ish@users.noreply.github.com>
2024-07-05 20:39:23 +0100

    Add files via upload

---
+++ client/main.js
@@ -0,0 +1,84 @@
+import { DiscordSDK } from "@discord/embedded-app-sdk";
+
+import rocketLogo from '/rocket.png';
+import "./style.css";
+
+// Will eventually store the authenticated user's access_token
+let auth;
+
+const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+
+
+
+async function appendVoiceChannelName() {
+  const app = document.querySelector('#app');
+
+  let activityChannelName = 'Unknown';
+
+  // Requesting the channel in GDMs (when the guild ID is null) requires
+  // the dm_channels.read scope which requires Discord approval.
+  if (discordSdk.channelId != null && discordSdk.guildId != null) {
+    // Over RPC collect info about the channel
+    const channel = await discordSdk.commands.getChannel({ channel_id: discordSdk.channelId });
+    if (channel.name != null) {
+      activityChannelName = channel.name;
+    }
+  }
+
+  // Update the UI with the name of the current voice channel
+  const textTagString = `Activity Channel: "${activityChannelName}"`;
+  const textTag = document.createElement('p');
+  textTag.textContent = textTagString;
+  app.appendChild(textTag);
+}
+
+async function setupDiscordSdk() {
+  await discordSdk.ready();
+  console.log("Discord SDK is ready");
+
+  // Authorize with Discord Client
+  const { code } = await discordSdk.commands.authorize({
+    client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
+    response_type: "code",
+    state: "",
+    prompt: "none",
+    scope: [
+      "identify",
+      "guilds",
+    ],
+  });
+
+  // Retrieve an access_token from your activity's server
+  const response = await fetch("/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      code,
+    }),
+  });
+  const { access_token } = await response.json();
+
+  // Authenticate with Discord client (using the access_token)
+  auth = await discordSdk.commands.authenticate({
+    access_token,
+  });
+
+  if (auth == null) {
+    throw new Error("Authenticate command failed");
+  }
+}
+
+setupDiscordSdk().then(() => {
+  console.log("Discord SDK is authenticated");
+
+  appendVoiceChannelName();
+});
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <img src="${rocketLogo}" class="logo" alt="Discord" />
+    <h1>Hello, World!</h1>
+  </div>
+`;
